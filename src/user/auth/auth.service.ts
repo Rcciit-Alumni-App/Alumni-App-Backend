50a93595ed1f32b1deb0b64369ad75dfc53f3e80
Subhadeep-coder <dharsubhadeep5@gmail.com>
2024-08-09 00:34:53 +0530

    modified:   src/user/auth/auth.service.ts

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -5,7 +5,6 @@ import { ConfigService } from '@nestjs/config';
 import { User, UserStatus, UserType } from '@prisma/client';
 import { LoginDto, ResetPasswordDto, SignupDto, UpdatePasswordDto, VerifyDto } from './dto';
 import { verifyAlumniStudent } from '../../../utils/auth/verify_alumni_student';
-import { InternalServerError } from '../../../utils/errors/server-error';
 import { generateOTP } from '../../../utils/auth/generate_otp';
 import { decodeToken } from '../../../utils/auth/decodeToken';
 import { MailerService } from '../../../src/mailer/mailer.service';
@@ -24,65 +23,65 @@ export class AuthService {
   async signup(signupDto: SignupDto) {
 
     // try {
-      // Get emails, password
-      const { college_email, college_roll, password, personal_email } = signupDto;
+    // Get emails, password
+    const { college_email, college_roll, password, personal_email } = signupDto;
 
-      // Check with database if any user exists or not
-      const existsUser = await this.prisma.user.findFirst({
-        where: {
-          college_mail: college_email,
-          college_roll,
-          personal_mail: personal_email,
-        },
-      });
-
-      if (existsUser) throw new UnauthorizedException('User already exists');
-      // generate the end year and detect the user type
-
-      const userType: {
-        user_type: UserType;
-        stream: string;
-      } = verifyAlumniStudent(college_email);
-
-      // Generate otp
-
-      const otp = generateOTP(6);
-
-      // Hash password
-      const hashedPassword = await argon.hash(password);
-      
-      // sign verification token
-      const payload = {
-        college_email,
-        personal_email,
+    // Check with database if any user exists or not
+    const existsUser = await this.prisma.user.findFirst({
+      where: {
+        college_mail: college_email,
         college_roll,
-        password: hashedPassword,
-        otp,
-        user_type: userType.user_type,
-        stream: userType.stream,
-      };
-      
-      const verificationToken = this.jwt.sign(payload, {
-        expiresIn: '5m',
-        secret: this.config.get('JWT_VERIFICATION_SECRET'),
-      });
-      
-      // Send email
-      
-      const mailData = {
-        otp: otp,
-      };
-      
-      await this.mailer.sendMail({
-        email: userType.user_type === 'ALUMNI' ? personal_email : college_email,
-        subject: 'Account Activation',
-        mail_file: 'verification_mail.ejs',
-        data: mailData,
-      });
-console.log(verificationToken);
-      return {
-        verificationToken,
-      };
+        personal_mail: personal_email,
+      },
+    });
+
+    if (existsUser) throw new UnauthorizedException('User already exists');
+    // generate the end year and detect the user type
+
+    const userType: {
+      user_type: UserType;
+      stream: string;
+    } = verifyAlumniStudent(college_email);
+
+    // Generate otp
+
+    const otp = generateOTP(6);
+
+    // Hash password
+    const hashedPassword = await argon.hash(password);
+
+    // sign verification token
+    const payload = {
+      college_email,
+      personal_email,
+      college_roll,
+      password: hashedPassword,
+      otp,
+      user_type: userType.user_type,
+      stream: userType.stream,
+    };
+
+    const verificationToken = this.jwt.sign(payload, {
+      expiresIn: '5m',
+      secret: this.config.get('JWT_VERIFICATION_SECRET'),
+    });
+
+    // Send email
+
+    const mailData = {
+      otp: otp,
+    };
+
+    await this.mailer.sendMail({
+      email: userType.user_type === 'ALUMNI' ? personal_email : college_email,
+      subject: 'Account Activation',
+      mail_file: 'verification_mail.ejs',
+      data: mailData,
+    });
+    console.log(verificationToken);
+    return {
+      verificationToken,
+    };
     // } catch (error) {
     //   console.log('[SIGNUP_ERROR]');
     // }
@@ -90,114 +89,111 @@ console.log(verificationToken);
   }
 
   async verify(verifyDto: VerifyDto) {
-      const { otp, verification_token } = verifyDto;
-      // Get token and otp
-      const decoded = this.jwt.verify(verification_token, {
-        secret: this.config.get('JWT_VERIFICATION_SECRET'),
-      });
+    const { otp, verification_token } = verifyDto;
+    // Get token and otp
+    const decoded = this.jwt.verify(verification_token, {
+      secret: this.config.get('JWT_VERIFICATION_SECRET'),
+    });
 
 
-      // Decode the token and check with otp
-      if (decoded.otp !== otp) throw new UnauthorizedException('Wrong OTP');
-      // Save the user to database
+    // Decode the token and check with otp
+    if (decoded.otp !== otp) throw new UnauthorizedException('Wrong OTP');
+    // Save the user to database
 
-      const user = await this.prisma.user.create({
-        data: {
-          full_name: '',
-          phone: '',
-          college_mail: decoded.college_email,
-          personal_mail: decoded.personal_email,
-          college_roll: decoded.college_roll,
-          domain: '',
-          password: decoded.password,
-          stream: decoded.stream,
-          status: UserStatus.ACCOUNT_DETAILS,
-          user_type: decoded.user_type,
-          profile_pic_url: '',
-          university_roll: '',
-        },
-      });
+    const user = await this.prisma.user.create({
+      data: {
+        full_name: '',
+        phone: '',
+        college_mail: decoded.college_email,
+        personal_mail: decoded.personal_email,
+        college_roll: decoded.college_roll,
+        domain: '',
+        password: decoded.password,
+        stream: decoded.stream,
+        status: UserStatus.ACCOUNT_DETAILS,
+        user_type: decoded.user_type,
+        profile_pic_url: '',
+        university_roll: '',
+      },
+    });
 
-      // Sign access_token and return
+    // Sign access_token and return
 
-      const access_token = await this.signAccessToken(
-        user.id,
-        user.personal_mail,
-        user.user_type,
-        false
-      );
+    const access_token = await this.signAccessToken(
+      user.id,
+      user.personal_mail,
+      user.user_type,
+      false
+    );
 
-      delete user.password;
-      // Save to redis
-      this.redis.setCache(user.id, user);
+    delete user.password;
+    // Save to redis
+    this.redis.setCache(user.id, user);
 
-      return {
-        access_token,
-      };
+    return {
+      access_token,
+    };
   }
 
   async resendOTP(token: string) {
-      const decoded = this.jwt.verify(token, {
-        secret: this.config.get('JWT_VERIFICATION_SECRET'),
-      });
+    const decoded = this.jwt.verify(token, {
+      secret: this.config.get('JWT_VERIFICATION_SECRET'),
+    });
 
-      const otp = generateOTP(6);
+    const otp = generateOTP(6);
 
-      const payload = { ...decoded, otp: otp };
+    const payload = { ...decoded, otp: otp };
 
-      const verificationToken = this.jwt.sign(payload, {
-        expiresIn: '5m',
-        secret: this.config.get('JWT_VERIFICATION_SECRET'),
-      });
+    const verificationToken = this.jwt.sign(payload, {
+      expiresIn: '5m',
+      secret: this.config.get('JWT_VERIFICATION_SECRET'),
+    });
 
-      // Send email
+    // Send email
 
-      const mailData = {
-        otp: otp,
-      };
+    const mailData = {
+      otp: otp,
+    };
 
-      await this.mailer.sendMail({
-        email: decoded.userType.user_type === 'ALUMNI' ? decoded.personal_email : decoded.college_email,
-        subject: 'Account Activation',
-        mail_file: 'verification_mail.ejs',
-        data: mailData,
-      });
+    await this.mailer.sendMail({
+      email: decoded.userType.user_type === 'ALUMNI' ? decoded.personal_email : decoded.college_email,
+      subject: 'Account Activation',
+      mail_file: 'verification_mail.ejs',
+      data: mailData,
+    });
 
-      return {
-        verificationToken,
-      };
+    return {
+      verificationToken,
+    };
   }
 
   async login(loginDto: LoginDto) {
-      const { personal_mail, password } = loginDto;
-      const user = await this.prisma.user.findFirst({
-        where: {
-          personal_mail: personal_mail,
-        },
-      });
-      if (!user) throw new UnauthorizedException('User does not exist !');
+    const { personal_mail, password } = loginDto;
+    const user = await this.prisma.user.findFirst({
+      where: {
+        personal_mail: personal_mail,
+      },
+    });
+    if (!user) throw new UnauthorizedException('User does not exist !');
 
 
-      const verified = await argon.verify(user.password, password);
-      if (!verified) throw new UnauthorizedException('Invalid Password !');
-      const access_token = await this.signAccessToken(
-        user.id,
-        user.personal_mail,
-        user.user_type,
-        false
-      );
+    const verified = await argon.verify(user.password, password);
+    if (!verified) throw new UnauthorizedException('Invalid Password !');
+    // TODO: FIx error
+    const access_token = await this.signAccessToken(
+      user.id,
+      user.personal_mail,
+      user.user_type,
+      false
+    );
 
-      delete user.password;
-      // Save to redis
-      this.redis.setCache(user.id, user);
+    delete user.password;
+    // Save to redis
+    this.redis.setCache(user.id, user);
 
-      return {
-        access_token,
-      };
-    } catch (error) {
-      console.log('[LOGIN_ERROR]');
-      throw new InternalServerError();
-    }
+    return {
+      access_token,
+    };
   }
 
   logout(token: string) {
@@ -206,98 +202,98 @@ console.log(verificationToken);
   }
 
   async forgotPassword(email: string) {
-      const existsUser = await this.prisma.user.findUnique({
-        where: {
-          personal_mail: email,
-        },
-      });
-
-      if (!existsUser) throw new UnauthorizedException('User does not exist');
-      const otp = generateOTP(6);
-      // sign verification token
-      const payload = {
-        user_id: existsUser.id,
+    const existsUser = await this.prisma.user.findUnique({
+      where: {
         personal_mail: email,
-        otp,
-        flag: 'F' // F -> forgot password, R -> reset password
-      };
+      },
+    });
 
-      const verificationToken = this.jwt.sign(payload, {
-        expiresIn: '5m',
-        secret: this.config.get('JWT_VERIFICATION_SECRET'),
-      });
+    if (!existsUser) throw new UnauthorizedException('User does not exist');
+    const otp = generateOTP(6);
+    // sign verification token
+    const payload = {
+      user_id: existsUser.id,
+      personal_mail: email,
+      otp,
+      flag: 'F' // F -> forgot password, R -> reset password
+    };
 
-      // Send email
+    const verificationToken = this.jwt.sign(payload, {
+      expiresIn: '5m',
+      secret: this.config.get('JWT_VERIFICATION_SECRET'),
+    });
 
-      const mailData = {
-        otp: otp,
-      };
+    // Send email
 
-      await this.mailer.sendMail({
-        email: email,
-        subject: 'Forgot Password',
-        mail_file: 'update_password_mail.ejs',
-        data: mailData,
-      });
+    const mailData = {
+      otp: otp,
+    };
 
-      return {
-        verificationToken,
-      };
+    await this.mailer.sendMail({
+      email: email,
+      subject: 'Forgot Password',
+      mail_file: 'update_password_mail.ejs',
+      data: mailData,
+    });
+
+    return {
+      verificationToken,
+    };
   }
 
   async resetPassword(token: string, resetPasswordDto: ResetPasswordDto) {
 
-      const { password, new_password } = resetPasswordDto;
+    const { password, new_password } = resetPasswordDto;
 
-      if (password === new_password) throw new UnauthorizedException("Password need to be different");
+    if (password === new_password) throw new UnauthorizedException("Password need to be different");
 
-      const userId = decodeToken(token, this.jwt, this.config);
-      const existsUser = await this.prisma.user.findUnique({
-        where: {
-          id: userId,
-        },
-      });
+    const userId = decodeToken(token, this.jwt, this.config);
+    const existsUser = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
 
-      if (!existsUser) throw new UnauthorizedException('User does not exist');
+    if (!existsUser) throw new UnauthorizedException('User does not exist');
 
-      const verified = await argon.verify(existsUser.password, password);
-      if (!verified) throw new UnauthorizedException('Invalid Password !');
+    const verified = await argon.verify(existsUser.password, password);
+    if (!verified) throw new UnauthorizedException('Invalid Password !');
 
-      const otp = generateOTP(6);
+    const otp = generateOTP(6);
 
 
-      const hashedPassword = await argon.hash(new_password);
+    const hashedPassword = await argon.hash(new_password);
 
-      // sign verification token
-      const payload = {
-        user_id: existsUser.id,
-        personal_mail: existsUser.personal_mail,
-        otp,
-        password: hashedPassword,
-        flag: 'R' // F -> forgot password, R -> reset password
-      };
+    // sign verification token
+    const payload = {
+      user_id: existsUser.id,
+      personal_mail: existsUser.personal_mail,
+      otp,
+      password: hashedPassword,
+      flag: 'R' // F -> forgot password, R -> reset password
+    };
 
-      const verificationToken = this.jwt.sign(payload, {
-        expiresIn: '5m',
-        secret: this.config.get('JWT_VERIFICATION_SECRET'),
-      });
+    const verificationToken = this.jwt.sign(payload, {
+      expiresIn: '5m',
+      secret: this.config.get('JWT_VERIFICATION_SECRET'),
+    });
 
-      // Send email
+    // Send email
 
-      const mailData = {
-        otp: otp,
-      };
+    const mailData = {
+      otp: otp,
+    };
 
-      await this.mailer.sendMail({
-        email: existsUser.personal_mail,
-        subject: 'Forgot Password',
-        mail_file: 'update_password_mail.ejs',
-        data: mailData,
-      });
+    await this.mailer.sendMail({
+      email: existsUser.personal_mail,
+      subject: 'Forgot Password',
+      mail_file: 'update_password_mail.ejs',
+      data: mailData,
+    });
 
-      return {
-        verificationToken,
-      };
+    return {
+      verificationToken,
+    };
   }
 
   async updatePassword(updatePasswordDto: UpdatePasswordDto) {
